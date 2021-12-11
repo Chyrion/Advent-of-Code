e752dc7cbf71bb37f515335700a9357899c9d092
Vili <laine.vili@gmail.com>
2021-12-11 13:09:28 +0200

    readme updated

--- 2021/day 9/problem1.py
+++ 2021/day 9/problem1.py
@@ -8,46 +8,8 @@
 input = [[int(x) for x in y] for y in (open('input.txt', 'r').read().splitlines())]
 lows = []
 
-def neighbors(n, x, y):
-  isLow = False
-  smallers = 4
-  try:
-    if n < input[x-1][y]:
-      smallers -= 1
-    if n < input[x+1][y]:
-      smallers -= 1
-    if n < input[x][y+1]:
-      smallers -= 1
-    if n < input[x][y-1]:
-      smallers -= 1
-  except:
-    smallers -= 1
-
-  if smallers == 0:
-    isLow = True
-    
-  return isLow
-  # if x == 0 and y == 0:
-  #   isLow = n < input[y+1][x] or n < input[y][x+1]
-  # elif x == 0:
-  #   isLow = n < input[y-1][x] or n < input[y+1][x] or n < input[y][x+1]
-  # elif x == len(input[0])-1:
-  #   isLow = n < input[y-1][x] or n < input[y+1][x] or n < input[y][x-1]
-  # elif y == 0:
-  #   isLow = n < input[y][x-1] or n < input[y][x+1] or n < input[y+1][x]
-  # elif y == len(input)-1:
-  #   isLow = n < input[y-1][x] or n < input[y][x+1] or n < input[y][x-1]
-  # elif x == len(input[0])-1 and y == len(input)-1:
-  #   isLow = n < input[y-1][x] or n < input[y][x-1]
-  # elif y == 0 and x == len(input[0])-1:
-  #   isLow = n < input[y][x-1] or n < input[y+1][x]
-  # elif x == 0 and y == len(input)-1:
-  #   isLow = n < input[y-1][x] or n < input[y][x+1]
+#for y in range(len(input)):
+#  for x in range(len(input[0])):
 
 
-for y in range(len(input)):
-  for x in range(len(input[0])):
-    if neighbors(input[y][x], x, y):
-      lows.append(input[y][x]+1)
 
-print(sum(lows))
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Advent of Code
 
-Repo of my Advent of Code solutions
+Repo of my Advent of Code solutions, done in Python 3. I'm a novice-intermediate level programmer, so the solutions I do may not be very optimal or pretty, but I'm improving on my skills day by day. Just don't expect anything incredibly advanced!
 
 My first AoC year is 2021, but I may solve problems from earlier years as well
+
+### Unsolved (2021):
+
+- Day 1, part 2
+- Day 3, part 2
+- Day 5
+- Day 8, part 2
+- Day 9
